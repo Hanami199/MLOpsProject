938e2ebd955dc94773d471ed6c7fee3e59de16a3
Davide <davide.zorzetto01@gmail.com>
2025-06-27 15:50:34 +0200

    git_commands.txt

--- ml/mlovie_train.py
+++ ml/mlovie_train.py
@@ -1,3 +1,21 @@
-from models import *
+from ml.models import *
+from sentence_transformers import SentenceTransformer
 
 
+def training(df_clean, selected, w):
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    mlovie = MLOVIE(model)
+    mlovie.fit(df_clean, selected, w)
+
+    mlovie.save()
+
+
+
+
+
+
+if __name__ == 'main':
+    selected =   ['title', 'director', 'cast', 'country', 'release_year',  'listed_in', 'description']
+    w = np.array([ .1,       0.4,        1,    0.1,       0.1,             2,            2])
+    df_clean = 0
+    training(selected, w)
