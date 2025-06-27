79508fe6e6a16193cd164f5722549e66a494d490
Davide <davide.zorzetto01@gmail.com>
2025-06-27 20:47:39 +0200

    refinement

--- test_unit.py
+++ test_unit.py
@@ -1,6 +1,6 @@
 from data.backend import ModelRecommendation
 
-mr = ModelRecommendation(model_type="MLOVIE", film_list_titles=["The Matrix", "Total Recall", "Bangkok Breaking", "Squid Game"])
+mr = ModelRecommendation(model_type="KNN", film_list_titles=["The Matrix", "Total Recall", "Bangkok Breaking", "Squid Game"])
 titles = mr.get_title()
 years = mr.get_year()
 print("Suggested Titles:")
